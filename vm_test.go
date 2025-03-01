d9727c8d38c1b7c20ff3a2c97d2a649ea4e1a85e
ZiomaleQ <r.partyka30@gmail.com>
2025-03-01 16:32:45 +0100

    update vm tests for tagged structs

--- vm_test.go
+++ vm_test.go
@@ -2,13 +2,38 @@ package parts
 
 import "testing"
 
-func TestHelper(t *testing.T) {
+func TestHelperNoTags(t *testing.T) {
 	type TestStruct struct {
 		Name string
 		Age  int
 	}
 
 	vm, err := GetVMWithSource("let Name = \"tfo\"; let Age = 22")
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	vm.Run()
+
+	var testStruct TestStruct
+
+	ReadFromParts(vm, &testStruct)
+
+	if testStruct.Age != 22 && testStruct.Name != "tfo" {
+		t.Errorf("inexpected values in the interface got (%d, %s) expected (22, \"tfo\")", testStruct.Age, testStruct.Name)
+		return
+	}
+}
+
+func TestHelperWithTags(t *testing.T) {
+	type TestStruct struct {
+		Name string `parts:"name"`
+		Age  int `parts:"age"`
+	}
+
+	vm, err := GetVMWithSource("let name = \"tfo\"; let age = 22")
 
 	if err != nil {
 		t.Error(err)
